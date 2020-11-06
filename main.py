360d0cb6595258defd1ea9c31d7f90c1b816a561
Mason W <mwhite24@biomedscienceacademy.org>
2020-11-06 14:29:23 +0000

    Made the Thing

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Mason White
+Period/Core: 6
 
 
 """
+"""
+Lets have some fun!
+
+Enter a Name!
+Enter a Fruit!
+Enter a Peice of Technology!
+Enter a different Name!
+Enter a Place!
+Enter a Color!
+"""
+print('Lets have some fun!')
+
+NAME = input("Enter a Name!")
+FRUIT = input("Enter a Fruit!")
+TECH = input("Enter a Peice of Technology!")
+NAME2 = input("Enter a Different Name!")
+PLACE = input("Enter a Place!")
+COLOR = input("Enter a Color!")
+
+"""
+[Name] was in [Place], goofing around, being silly.
+Before [Name] could react, [Name 2] was here, talking to [Name]
+"Hello! [Name], I see you have a new, [Color] [Peice of Technology]!"
+Name 2 was eating a [Fruit]
+"""
+print(NAME, "was in", PLACE, ", goofing around, being silly.", "Before", NAME, "Could react,", NAME2, "was here, talking to", NAME, '"Hello!', NAME, "I see you have a new,", COLOR, TECH, '!"', NAME2, "was eating a", FRUIT)
